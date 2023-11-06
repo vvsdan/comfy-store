23d06b06e64caacc97c4e8ecfe1012b925c3d528
Daniel Jones <vvsdan999@gmail.com>
2023-11-06 17:19:58 -0500

    render feature products section title & products grid placeholder

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -1,6 +1,12 @@
+import ProductsGrid from "./ProductsGrid";
+import SectionTitle from "./SectionTitle";
+
 const FeaturedProducts = () => {
   return (
-    <div>FeaturedProducts</div>
-  )
-}
-export default FeaturedProducts
+    <div className="pt-24">
+      <SectionTitle text="featured products" />
+      <ProductsGrid />
+    </div>
+  );
+};
+export default FeaturedProducts;
