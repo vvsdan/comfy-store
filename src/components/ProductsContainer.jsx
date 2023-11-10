3fead8d9d72c1222fe7e840270f8d8df3393fd4b
Daniel Jones <vvsdan999@gmail.com>
2023-11-09 23:15:29 -0500

    add grid and list buttons

--- src/components/ProductsContainer.jsx
+++ src/components/ProductsContainer.jsx
@@ -1,9 +1,42 @@
+import { useLoaderData } from "react-router-dom";
+import { useState } from "react";
+import { BsFillGridFill, BsList } from "react-icons/bs";
+
 import ProductsGrid from "./ProductsGrid";
 import ProductsList from "./ProductsList";
 
 const ProductsContainer = () => {
+  const { meta } = useLoaderData();
+  const totalProducts = meta.pagination.total;
+  const [layout, setLayout] = useState("grid");
+
+  const setActiveStyles = (pattern) => {
+    return `text-xl btn btn-circle btn-sm`;
+  };
   return (
     <>
+      {/* HEADER */}
+      <div className="flex justify-between items-center mt-8 border-b border-base-300 pb-5"></div>
+      <h4 className="font-medium text-md">
+        {totalProducts} product {totalProducts > 1 && "s"}
+      </h4>
+      <div className="flex gap-x-2">
+        <button
+          type="button"
+          onClick={() => setLayout("grid")}
+          className={setActiveStyles("grid")}
+        >
+          <BsFillGridFill />
+        </button>
+        <button
+          type="button"
+          onClick={() => setLayout("list")}
+          className={setActiveStyles("list")}
+        >
+          <BsList />
+        </button>
+      </div>
+      {/* PRODUCTS */}
       <ProductsList />
       <ProductsGrid />
     </>
