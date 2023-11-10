3c216008ee9eb590fa1c458e556cb6278a3f1f78
Daniel Jones <vvsdan999@gmail.com>
2023-11-10 16:12:46 -0500

    add base filters form component

---
+++ src/components/Filters.jsx
@@ -0,0 +1,12 @@
+import { Form, useLoaderData, Link } from "react-router-dom";
+import FormInput from "./FormInput";
+
+const Filters = () => {
+  return (
+    <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
+      {/* SEARCH */}
+      {/* BUTTONS */}
+    </Form>
+  );
+};
+export default Filters;
