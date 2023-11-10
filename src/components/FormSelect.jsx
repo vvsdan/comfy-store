8720c922d54c167b2ef64dc1cfb12611b3f2ec92
Daniel Jones <vvsdan999@gmail.com>
2023-11-10 16:49:49 -0500

    bugfix: size of filter selection

--- src/components/FormSelect.jsx
+++ src/components/FormSelect.jsx
@@ -3,21 +3,21 @@ const FormSelect = ({ label, name, list, defaultValue, size }) => {
     <div className="form-control">
       <label htmlFor={name} className="label">
         <span className="label-text capitalize">{label}</span>
-        <select
-          name={name}
-          id={name}
-          className={`select select-bordered`}
-          defaultValue={defaultValue}
-        >
-          {list.map((item) => {
-            return (
-              <option key={item} value={item}>
-                {item}
-              </option>
-            );
-          })}
-        </select>
       </label>
+      <select
+        name={name}
+        id={name}
+        className={`select select-bordered ${size}`}
+        defaultValue={defaultValue}
+      >
+        {list.map((item) => {
+          return (
+            <option key={item} value={item}>
+              {item}
+            </option>
+          );
+        })}
+      </select>
     </div>
   );
 };
