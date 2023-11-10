3c0f06ff3a80439270440f8f44e74d0a3f6649e4
Daniel Jones <vvsdan999@gmail.com>
2023-11-10 16:14:08 -0500

    add search and reset buttons to form

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -6,6 +6,12 @@ const Filters = () => {
     <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
       {/* SEARCH */}
       {/* BUTTONS */}
+      <button type="submit" className="btn btn-primary btn-sm">
+        search
+      </button>
+      <Link to="/products" className="btn btn-accent btn-sm">
+        reset
+      </Link>
     </Form>
   );
 };
