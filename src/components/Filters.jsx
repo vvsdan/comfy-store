e31475b268ae225019231a18d956ede8e78a7479
Daniel Jones <vvsdan999@gmail.com>
2023-11-10 16:18:05 -0500

    add product search field

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -5,6 +5,12 @@ const Filters = () => {
   return (
     <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
       {/* SEARCH */}
+      <FormInput
+        type="search"
+        label="search product"
+        name="search"
+        size="input-sm"
+      ></FormInput>
       {/* BUTTONS */}
       <button type="submit" className="btn btn-primary btn-sm">
         search
