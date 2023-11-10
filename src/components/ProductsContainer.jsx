b0313aa59abfe060c9708de125bf2c9ae90c2b78
Daniel Jones <vvsdan999@gmail.com>
2023-11-09 22:44:03 -0500

    add list of products to products container

--- src/components/ProductsContainer.jsx
+++ src/components/ProductsContainer.jsx
@@ -1,8 +1,10 @@
 import ProductsGrid from "./ProductsGrid";
+import ProductsList from "./ProductsList";
 
 const ProductsContainer = () => {
   return (
     <>
+      <ProductsList />
       <ProductsGrid />
     </>
   );
