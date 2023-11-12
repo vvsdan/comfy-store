4c99c34223853b72486e5cd04ace19997ec9660d
Daniel Jones <vvsdan999@gmail.com>
2023-11-11 23:54:13 -0500

    bugfix: grid/list button alignment

--- src/components/ProductsContainer.jsx
+++ src/components/ProductsContainer.jsx
@@ -20,25 +20,26 @@ const ProductsContainer = () => {
   return (
     <>
       {/* HEADER */}
-      <div className="flex justify-between items-center mt-8 border-b border-base-300 pb-5"></div>
-      <h4 className="font-medium text-md">
-        {totalProducts} product{totalProducts > 1 && "s"}
-      </h4>
-      <div className="flex gap-x-2">
-        <button
-          type="button"
-          onClick={() => setLayout("grid")}
-          className={setActiveStyles("grid")}
-        >
-          <BsFillGridFill />
-        </button>
-        <button
-          type="button"
-          onClick={() => setLayout("list")}
-          className={setActiveStyles("list")}
-        >
-          <BsList />
-        </button>
+      <div className="flex justify-between items-center mt-8 border-b border-base-300 pb-5">
+        <h4 className="font-medium text-md">
+          {totalProducts} product{totalProducts > 1 && "s"}
+        </h4>
+        <div className="flex gap-x-2">
+          <button
+            type="button"
+            onClick={() => setLayout("grid")}
+            className={setActiveStyles("grid")}
+          >
+            <BsFillGridFill />
+          </button>
+          <button
+            type="button"
+            onClick={() => setLayout("list")}
+            className={setActiveStyles("list")}
+          >
+            <BsList />
+          </button>
+        </div>
       </div>
       {/* PRODUCTS */}
       <div>
