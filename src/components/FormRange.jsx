38cbe1881924fcb9af7e7a295cdae4e491efb5eb
Daniel Jones <vvsdan999@gmail.com>
2023-11-11 23:27:56 -0500

    create form range component and set up useState

---
+++ src/components/FormRange.jsx
@@ -0,0 +1,8 @@
+import { formatPrice } from "../utils";
+const FormRange = ({ label, name, size }) => {
+  const step = 1000;
+  const maxPrice = 100000;
+  const [selectedPrice, setSelectedPrice] = useState(maxPrice);
+  return <div>FormRange</div>;
+};
+export default FormRange;
