f1bd34b1b055f8329f960ca2859bdcf3c2cf1e45
Daniel Jones <vvsdan999@gmail.com>
2023-11-13 14:55:03 -0500

    set up query search params

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,6 +3,9 @@ import { customFetch } from "../utils";
 const url = "/products";
 
 export const loader = async ({ request }) => {
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ]);
   const response = await customFetch(url);
   const products = response.data.data;
   const meta = response.data.meta;
