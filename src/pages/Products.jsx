19ba872cbddf226fe8d1ed246d10ef84ea821b37
Daniel Jones <vvsdan999@gmail.com>
2023-11-13 21:16:33 -0500

    complete working filters for each param

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -6,10 +6,11 @@ export const loader = async ({ request }) => {
   const params = Object.fromEntries([
     ...new URL(request.url).searchParams.entries(),
   ]);
-  const response = await customFetch(url);
+
+  const response = await customFetch(url, { params });
   const products = response.data.data;
   const meta = response.data.meta;
-  return { products, meta };
+  return { products, meta, params };
 };
 
 const Products = () => (
