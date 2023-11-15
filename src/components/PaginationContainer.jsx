08f180bd3a44f9bb8cecd910a18eec57f6e9fdff
Daniel Jones <vvsdan999@gmail.com>
2023-11-15 13:05:50 -0500

    feature: complete working pagination via clicking page numbers

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -7,8 +7,12 @@ const PaginationContainer = () => {
   const pages = Array.from({ length: pageCount }, (_, index) => {
     return index + 1;
   });
+  const { search, pathname } = useLocation();
+  const navigate = useNavigate();
   const handlePageChange = (pageNumber) => {
-    console.log(pageNumber);
+    const searchParams = new URLSearchParams(search);
+    searchParams.set("page", pageNumber);
+    navigate(`${pathname}?${searchParams.toString()}`);
   };
 
   if (pageCount < 2) return null;
@@ -28,7 +32,7 @@ const PaginationContainer = () => {
             <button
               key={pageNumber}
               onClick={() => handlePageChange(pageNumber)}
-              className={`btn btn-s sm:btn-med border-none join-item ${
+              className={`btn btn-xs sm:btn-md border-none join-item ${
                 pageNumber === page ? "bg-base-300 border-base-300" : ""
               }`}
             >
