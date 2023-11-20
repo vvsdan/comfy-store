dde0f2433aa070b6feebea0076be3cc7e03de66e
Daniel Jones <vvsdan999@gmail.com>
2023-11-20 11:52:05 -0500

    feature: edit item reducer functionality

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -46,7 +46,15 @@ const cartSlice = createSlice({
       cartSlice.caseReducers.calculateTotals(state);
       toast.error("Item removed from cart");
     },
-    editItem: (state, action) => {},
+    editItem: (state, action) => {
+      const { cartID, amount } = action.payload;
+      const item = state.cartItems.find((i) => i.cartID === cartID);
+      state.numItemsInCart += amount - item.amount;
+      state.cartTotal += item.price * (amount - item.amount);
+      item.amount = amount;
+      cartSlice.caseReducers.calculateTotals(state);
+      toast.success("Cart updated");
+    },
     calculateTotals: () => {
       state.tax = 0.1 * state.cartTotal;
       state.orderTotal = state.cartTotal + state.shipping + state.tax;
