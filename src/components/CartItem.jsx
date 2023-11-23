23c04f5ac7dfc275e166420f988e35c09ff05029
Daniel Jones <vvsdan999@gmail.com>
2023-11-22 23:47:57 -0500

    add images to cart item

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,4 +1,26 @@
+import { formatPrice, generateAmountOptions } from "../utils";
+import { removeItems, editItem } from "../features/cart/cartSlice";
+import { useDispatch } from "react-redux";
+
 const CartItem = () => {
-  return <div>CartItem</div>;
+  const { cartID, title, price, image, amount, company, productColor } =
+    CartItem;
+  return (
+    <article
+      key={cartID}
+      className="mb-12 flex flex-col gap-y-4 sm:flex-row flex-wrap border-b border-base-300 pb-6 last:border-b-0"
+    >
+      {/* IMAGE */}
+      <img
+        src={image}
+        alt={title}
+        className="h-24 w-24 rounded-lg sm:h-32 object-cover"
+      />
+      {/* COLOR */}
+      {/* AMOUNT */}
+      {/* REMOVE */}
+      {/* PRICE */}
+    </article>
+  );
 };
 export default CartItem;
