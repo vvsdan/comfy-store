7a4274ff22a1656c629603a5b9881c5827905dc2
Daniel Jones <vvsdan999@gmail.com>
2023-12-01 23:28:20 -0500

    add logout user logic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,28 @@
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
+import { clearCart } from "../features/cart/cartSlice";
+import { logoutUser } from "../features/user/userSlice";
 
 const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const user = useSelector((state) => state.userState.user);
+
+  const handleLogout = () => {
+    navigate("/");
+    dispatch(clearCart());
+    dispatch(logoutUser());
+  };
   return (
     <header className="bg-neutral py-2 text-neutral-content">
       <div className="align-element flex justify-center sm:justify-end">
         {user ? (
           <div className="flex gap-x-2 sm:gap-x-8 items-center">
             <p className="text-xs sm:text-sm">Hello, {user.username}</p>
-            <button className="btn btn-xs btn-outline btn-primary">
+            <button
+              className="btn btn-xs btn-outline btn-primary"
+              onClick={handleLogout}
+            >
               logout
             </button>
           </div>
