52db5dc0676ebc240e4b44622b53980f82dcc29e
Daniel Jones <vvsdan999@gmail.com>
2023-12-01 23:25:09 -0500

    create user greeting if logged in, add logout button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,32 @@
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.userState.user);
   return (
     <header className="bg-neutral py-2 text-neutral-content">
       <div className="align-element flex justify-center sm:justify-end">
+        {user ? (
+          <div className="flex gap-x-2 sm:gap-x-8 items-center">
+            <p className="text-xs sm:text-sm">Hello, {user.username}</p>
+            <button className="btn btn-xs btn-outline btn-primary">
+              logout
+            </button>
+          </div>
+        ) : (
+          <div className="flex gap-x-6 justify-center items-center">
+            <Link to="/login" className="link link-hover text-xs sm:text-sm">
+              Sign in / Guest
+            </Link>
+            <Link to="/register" className="link link-hover text-xs sm:text-sm">
+              Create Account
+            </Link>
+          </div>
+        )}
         {/* USER */}
         {/* LINKS */}
-        <div className="flex gap-x-6 justify-center items-center">
-          <Link to="/login" className="link link-hover text-xs sm:text-sm">
-            Sign in / Guest
-          </Link>
-          <Link to="/register" className="link link-hover text-xs sm:text-sm">
-            Create Account
-          </Link>
-        </div>
       </div>
     </header>
   );
