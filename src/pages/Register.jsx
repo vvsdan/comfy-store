3b1fd4d6e02dd89ac0296ad245ca8f19fde58797
Daniel Jones <vvsdan999@gmail.com>
2023-12-02 23:44:53 -0500

    add placeholder default value for register name, email, and pass

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import { FormInput, SubmitBtn } from "../components";
 import { Form, Link } from "react-router-dom";
 
-export const action = async () => {
+export const action = async ({ request }) => {
   return null;
 };
 
@@ -13,9 +13,24 @@ const Register = () => {
         className="card w-96 p-8 bg-base-100 shadow-log flex flex-col gap-y-4"
       >
         <h4 className="text-center text-3xl font-bold">Register</h4>
-        <FormInput type="text" label="username" name="username" />
-        <FormInput type="email" label="email" name="email" />
-        <FormInput type="password" label="password" name="password" />
+        <FormInput
+          type="text"
+          label="username"
+          name="username"
+          defaultValue="dan1 test"
+        />
+        <FormInput
+          type="email"
+          label="email"
+          name="email"
+          defaultValue="dan1test@gmail.com"
+        />
+        <FormInput
+          type="password"
+          label="password"
+          name="password"
+          defaultValue="secret"
+        />
         <div className="mt-4">
           <SubmitBtn text="register" />
         </div>
