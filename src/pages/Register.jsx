861f5df50963a4fa1af3795518e511de872969e1
Daniel Jones <vvsdan999@gmail.com>
2023-12-02 23:51:53 -0500

    complete register action functionality with async req

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,22 @@
 import { FormInput, SubmitBtn } from "../components";
 import { Form, Link } from "react-router-dom";
+import { customFetch } from "../utils";
+import { toast } from "react-toastify";
 
 export const action = async ({ request }) => {
-  return null;
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  try {
+    const response = await customFetch.post("/auth/local/register", data);
+    toast.success("account created successfully");
+    return redirect("/login");
+  } catch (error) {
+    const errorMessage =
+      error?.response?.data?.error?.message ||
+      "please double check your credentials";
+    toast.error(errorMessage);
+    return null;
+  }
 };
 
 const Register = () => {
