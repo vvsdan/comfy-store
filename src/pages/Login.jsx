c644a292226666c6d593d95dbf67e1c91e90766e
Daniel Jones <vvsdan999@gmail.com>
2023-12-04 22:58:36 -0500

    complete login action

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,9 +5,24 @@ import { toast } from "react-toastify";
 import { loginUser } from "../features/user/userSlice";
 import { useDispatch } from "react-redux";
 
-export const action = async (store) => {
-  return null;
-};
+export const action =
+  (store) =>
+  async ({ request }) => {
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData);
+    try {
+      const response = await customFetch.post("/auth/local/", data);
+      store.dispatch(loginUser(response.data));
+      toast.success("logged in successfully");
+      return redirect("/");
+    } catch (error) {
+      const errorMessage =
+        error?.response?.data?.error?.message ||
+        "please double check your credentials";
+      toast.error(errorMessage);
+      return null;
+    }
+  };
 
 const Login = () => {
   return (
