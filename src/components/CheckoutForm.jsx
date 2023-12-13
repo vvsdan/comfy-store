eedb47801abedd06d0ac094175e2f06fdd41deeb
Daniel Jones <vvsdan999@gmail.com>
2023-12-12 23:41:39 -0500

    add checkout form

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,4 +1,25 @@
+import { Form, redirect } from "react-router-dom";
+import FormInput from "./FormInput";
+import SubmitBtn from "./SubmitBtn";
+import { customFetch, formatPrice } from "../utils";
+import { toast } from "./react-toastify";
+import { clearCart } from "../features/cart/cartSlice";
+
+export const action = (store) => async () => {
+  console.log(store);
+  return null;
+};
+
 const CheckoutForm = () => {
-  return <div>CheckoutForm</div>;
+  return (
+    <Form method="POST" className="flex flex-col gay-y-4">
+      <h4 className="font-medium text-xl capitalize">shipping information</h4>
+      <FormInput label="first name" name="name" type="text" />
+      <FormInput label="address" name="address" type="text" />
+      <div className="mt-4">
+        <SubmitBtn text="place your order" />
+      </div>
+    </Form>
+  );
 };
 export default CheckoutForm;
