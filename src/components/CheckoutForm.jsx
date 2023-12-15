50ca44e47cf8029e80089cfe1b75a885b09befc8
Daniel Jones <vvsdan999@gmail.com>
2023-12-14 23:42:27 -0500

    add formData request action

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -5,10 +5,16 @@ import { customFetch, formatPrice } from "../utils";
 import { toast } from "./react-toastify";
 import { clearCart } from "../features/cart/cartSlice";
 
-export const action = (store) => async () => {
-  console.log(store);
-  return null;
-};
+export const action =
+  (store) =>
+  async ({ request }) => {
+    const formData = await request.formData();
+    const { name, address } = Object.fromEntries(formData);
+    const user = store.getState().userState.user;
+    const { cartItems, orderTotal, numItemsInCart } =
+      store.getState().cartState;
+    return null;
+  };
 
 const CheckoutForm = () => {
   return (
